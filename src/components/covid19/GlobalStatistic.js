a76c0b9222ac53afa72f650b737971ad09ba0591
Tiger Chen <tigerchen@Tigers-MBP-2.modem>
2020-07-01 22:41:36 +1000

    m

--- src/components/covid19/GlobalStatistic.js
+++ src/components/covid19/GlobalStatistic.js
@@ -2,14 +2,17 @@ import React from 'react';
 import '../../style/GlobalStatistic.css';
 
 function GlobalStatistic({ data }) {
-  const {cases, active, deaths, recovered} = data;
+  const {cases, active, deaths, recovered, updated} = data;
   return (
-    <div className="globalStats-grid">
-      <div>Cases:<span className="red">{new Intl.NumberFormat().format(cases)}</span></div>
-      <div>Deaths: <span className="gray">{new Intl.NumberFormat().format(deaths)}</span></div>
-      <div>Active: <span className="orange">{new Intl.NumberFormat().format(active)}</span></div>
-      <div>Recovered: <span className="green">{new Intl.NumberFormat().format(recovered)}</span></div>
-    </div>
+    <>
+      <h5 className="center">Updated on {new Date(updated).toLocaleString()}</h5>
+      <div className="globalStats-grid">
+        <div>Cases:<span className="red">{new Intl.NumberFormat().format(cases)}</span></div>
+        <div>Deaths: <span className="gray">{new Intl.NumberFormat().format(deaths)}</span></div>
+        <div>Active: <span className="orange">{new Intl.NumberFormat().format(active)}</span></div>
+        <div>Recovered: <span className="green">{new Intl.NumberFormat().format(recovered)}</span></div>
+      </div>
+    </>
   )
 }
 
